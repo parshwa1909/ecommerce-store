b7cd0be4fa1776ca5eaec9655c1bb5aa7f5fd3b2
parshwa1909 <130059845+parshwa1909@users.noreply.github.com>
2023-06-21 15:52:53 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv/config");
 
-return mongoose.connect(
-  process.env.DB_CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("conected to db")
-);
+mongoose.connect(
+  "mongodb+srv://parshwa2001:P$g8460887627@cluster0.nk7vwwl.mongodb.net/<database-name>?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log("Connected to the database"))
+  .catch((error) => console.error("Failed to connect to the database:", error));
